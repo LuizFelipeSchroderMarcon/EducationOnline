ac51ccbf368768c7b7cfa1c66507941ee27ab42b
Luiz Felipe Schroder Marcon <112869474+LuizFelipeSchroderMarcon@users.noreply.github.com>
2022-10-26 08:51:16 -0300

    Primeira versão do projeto
    Esse projeto inicial no momento que foi criado estamos criando uma API REST com dados mockados.

---
+++ plataforma-educacional/js/conectaApi.js
@@ -0,0 +1,17 @@
+async function listaVideos(){
+
+    const conexao = await fetch("http://localhost:3000/videos")
+
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+
+   // console.log(conexaoConvertida);
+}
+
+
+export const conectaApi ={
+    listaVideos
+}
+
+
